2a53886e5814a49c9998a215d883e3552e16efb2
Simian Wang <879837946@qq.com>
2019-05-02 05:23:58 +1000

    finished question 2

--- src/Q2Add.java
+++ src/Q2Add.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Q2Add {
     public static class ListNode {
@@ -11,193 +12,63 @@ public class Q2Add {
     }
 
     public static void main(String[] args) {
-        ListNode t1 = new ListNode(1);
-        t1.next = new ListNode(0);
-        ListNode t2 = new ListNode(2);
-        t2.next = new ListNode(3);
-        ListNode test = addTwoNumbers(t1,t2);
-        System.out.println(lengthList(test));
+
     }
 
-    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ArrayList<Integer> answer = new ArrayList<>();
-        ListNode currentOne = l1;
-        ListNode currentTwo = l2;
-        int addOn = 0;
-        //先把最简单的情况排除出去
-        if (l1 == null) {
-            return l2;
-        } else if (l2 == null) {
-            return l1;
-        }
-        if (lengthList(currentOne) > lengthList(currentTwo)) {
-            //先循环处理l2最后一位之前的东西。
-            while (currentTwo.next != null) {
-                int thisDigit = currentOne.val + currentTwo.val + addOn;
-                if (thisDigit >= 10) {
-                    //进一位
-                    addOn = 1;
-                    //减去10
-                    int thisNum = thisDigit - 10;
-                    answer.add(thisNum);
-                    currentOne = currentOne.next;
-                    currentTwo = currentTwo.next;
-                } else {
-                    addOn = 0;
-                    answer.add(thisDigit);
-                    currentOne = currentOne.next;
-                    currentTwo = currentTwo.next;
-                }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        // answer用来装答案
+        ListNode answer = new ListNode(0);
+        // one是指向l1不同位置的指针。two同理
+        ListNode one = l1;
+        ListNode two = l2;
+        // current用于指向我现在想操作的answer部分。
+        ListNode current = answer;
+        int carry = 0;
+        while (one != null || two != null) {
+            int oneValue = (one != null) ? one.val : 0;
+            int twoValue = (two != null) ? two.val : 0;
+            int sum = carry + oneValue + twoValue;
+            //整数除整数，必定留carry
+            carry = sum / 10;
+            //因为我们第一位已经占住了一个，所以永远都拿后一位来存这一位发生的事。
+            current.next = new ListNode(sum % 10);
+            //把指针往后指一位。
+            current = current.next;
+            if (one != null) {
+                one = one.next;
             }
-            //处理l2最后一位
-            //rideOn是进位
-            int rideOn = 0;
-            int thisDigit = currentTwo.val + currentOne.val + addOn;
-            if (thisDigit >= 10) {
-                rideOn = 1;
-                thisDigit -= 10;
-                answer.add(thisDigit);
-            } else {
-                answer.add(thisDigit);
-            }
-            currentOne = currentOne.next;
-            //安排完了进位，继续安排l1，直到l1最后一位。
-            while (currentOne.next != null) {
-                if (rideOn == 1) {
-                    thisDigit = currentOne.val + rideOn;
-                    if (thisDigit >= 10) {
-                        thisDigit -= 10;
-                        answer.add(thisDigit);
-                        currentOne = currentOne.next;
-                    } else {
-                        rideOn = 0;
-                        answer.add(thisDigit);
-                        currentOne = currentOne.next;
-                    }
-                } else {
-                    answer.add(currentOne.val);
-                    currentOne = currentOne.next;
-                }
-            }
-            //到了最后一位，随便处理一下即可。
-            if (rideOn == 1) {
-                thisDigit = rideOn + currentOne.val;
-                if (thisDigit == 10) {
-                    answer.add(0);
-                    answer.add(1);
-                }
-            } else {
-                answer.add(currentOne.val);
-            }
-        } else if (lengthList(currentTwo) > lengthList(currentOne)) {
-            //先循环处理l1最后一位之前的东西。
-            while (currentOne.next != null) {
-                int thisDigit = currentOne.val + currentTwo.val + addOn;
-                if (thisDigit >= 10) {
-                    //进一位
-                    addOn = 1;
-                    //减去10
-                    int thisNum = thisDigit - 10;
-                    answer.add(thisNum);
-                    currentOne = currentOne.next;
-                    currentTwo = currentTwo.next;
-                } else {
-                    addOn = 0;
-                    answer.add(thisDigit);
-                    currentOne = currentOne.next;
-                    currentTwo = currentTwo.next;
-                }
-            }
-            //处理l1最后一位
-            //rideOn是进位
-            int rideOn = 0;
-            int thisDigit = currentTwo.val + currentOne.val + addOn;
-            if (thisDigit >= 10) {
-                rideOn = 1;
-                thisDigit -= 10;
-                answer.add(thisDigit);
-            } else {
-                answer.add(thisDigit);
-            }
-            currentTwo = currentTwo.next;
-            //安排完了进位，继续安排l1，直到l1最后一位。
-            while (currentTwo.next != null) {
-                if (rideOn == 1) {
-                    thisDigit = currentTwo.val + rideOn;
-                    if (thisDigit >= 10) {
-                        thisDigit -= 10;
-                        answer.add(thisDigit);
-                        currentTwo = currentTwo.next;
-                    } else {
-                        rideOn = 0;
-                        answer.add(thisDigit);
-                        currentTwo = currentTwo.next;
-                    }
-                } else {
-                    answer.add(currentTwo.val);
-                    currentTwo = currentTwo.next;
-                }
-            }
-            //到了最后一位，随便处理一下即可。
-            if (rideOn == 1) {
-                thisDigit = rideOn + currentTwo.val;
-                if (thisDigit == 10) {
-                    answer.add(0);
-                    answer.add(1);
-                }
-            } else {
-                answer.add(currentTwo.val);
-            }
-        } else {
-            //l1 l2等长，那么随便搞搞就行了。
-            //先把最后一位之前的都安排好了。
-            while (currentOne.next != null) {
-                int thisDigit = currentOne.val + currentTwo.val + addOn;
-                if (thisDigit >= 10) {
-                    addOn = 1;
-                    thisDigit -= 10;
-                    answer.add(thisDigit);
-                } else {
-                    addOn = 0;
-                    answer.add(thisDigit);
-                }
-                currentOne = currentOne.next;
-                currentTwo = currentTwo.next;
-            }
-            //来处理最后一位
-            int lastDigit = currentOne.val + currentTwo.val + addOn;
-            if (lastDigit >= 10) {
-                int lastTwo = lastDigit - 10;
-                answer.add(lastTwo);
-                answer.add(1);
-            } else {
-                answer.add(lastDigit);
+            if (two != null) {
+                two = two.next;
             }
         }
-        return combine(answer);
+        //当我们走出上面这个循环的时候，这就说明，两个input的所有位的相加已经都处理完了。
+        //唯一有可能没处理的就是，最后一位相加产生carry，那么那个carry还没被加到后面。
+        //那么如果确实有这么一个carry位，把他加到后面去就可以了。
+        if (carry == 1) {
+            current.next = new ListNode(carry);
+        }
+        //这里千万不能忘了，把最开始的那个0去掉。
+        return answer.next;
     }
 
-    public static int lengthList(ListNode listNode) {
-        int length = 0;
-        if (listNode == null) {
-            return 0;
-        }
-        while (listNode.next != null) {
-            length ++;
-            listNode = listNode.next;
-        }
-        return length;
-    }
+//    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//        ListNode dummyHead = new ListNode(0);
+//        ListNode p = l1, q = l2, curr = dummyHead;
+//        int carry = 0;
+//        while (p != null || q != null) {
+//            int x = (p != null) ? p.val : 0;
+//            int y = (q != null) ? q.val : 0;
+//            int sum = carry + x + y;
+//            carry = sum / 10;
+//            curr.next = new ListNode(sum % 10);
+//            curr = curr.next;
+//            if (p != null) p = p.next;
+//            if (q != null) q = q.next;
+//        }
+//        if (carry > 0) {
+//            curr.next = new ListNode(carry);
+//        }
+//        return dummyHead.next;
+//    }
 
-    public static ListNode combine(ArrayList<Integer> input) {
-        if (input.size() == 0) {
-            return null;
-        }
-        ListNode answer = new ListNode(input.get(0));
-        for (int i = 1; i < input.size(); i++) {
-            answer.next = new ListNode(input.get(i));
-            answer = answer.next;
-        }
-        return answer;
-    }
 }
